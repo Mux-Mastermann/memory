135662708c7e46e2a23b93a01cc0b88592f45099
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2020-05-25 00:22:02 +0200

    Refactored, added images, size increased

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,48 +1,79 @@
-const cards = document.querySelectorAll(".image");
+// defining constants
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg", "17.jpg", "18.jpg", "19.jpg", "20.jpg", "21.jpg", "22.jpg", "23.jpg", "24.jpg", "25.jpg", "26.jpg", "27.jpg", "28.jpg", "29.jpg", "30.jpg", "31.jpg", "32.jpg", "33.jpg", "34.jpg", "35.jpg", "36.jpg"]
+const boardSize = [3, 4]
+const cards = document.querySelectorAll(".card");
 
-let firstCard, secondCard, wait, flipped
+// define global variables
+var firstCard, secondCard, wait, flipped
 
+// start new game on page loading
 newGame()
 
 function newGame() {
-    var order = [1, 2, 3, 4, 1, 2, 3, 4]
+    // initialize variables
     wait = false
     flipped = false
+    // unflip all images
     unflipAllImages();
-    randomNameImages(order);
+    // shuffle the board
+    shuffleBoard();
+    // add event listeners to cards and make all visible
     cards.forEach(card => {
         card.style.visibility = 'visible'
-        card.addEventListener("click", anotherRound)
+        card.addEventListener("click", turn)
     })
 }
 
-function randomNameImages(order) {
+function shuffleBoard() {
+    // how many images are needed for board
+    imageNo = (boardSize[0] * boardSize[1]) / 2
+    // shuffle images array for random slice
+    const shuffled = images.sort(() => 0.5 - Math.random());
+    // slice imageNo images from images 
+    imageSet = shuffled.slice(0, imageNo);
+    // store used images from imageSet here
+    let usedImages = new Array();
+    // set start value for card id
+    card_id = 1;
+    // give each card a name of a image (each image name exists twice)
     cards.forEach(function (card) {
-        var index = Math.floor(Math.random() * order.length)
-        card.setAttribute("name", order[index])
-        order.splice(index, 1)
+        // get random index number for imageSet array
+        var index = Math.floor(Math.random() * imageSet.length)
+        // store image on that index in img
+        let img = imageSet[index]
+        // set name of card to the image name
+        card.setAttribute("name", img)
+        // set card id
+        card.setAttribute("id", card_id)
+        // check if image was already used once and delete from imageSet if so
+        if (usedImages.includes(img)) {
+            imageSet.splice(index, 1);
+        }
+        // push image to used Images array
+        usedImages.push(img)
+        // increase card id
+        card_id++;
     })
 }
 
-function unflipAllImages() {
-    cards.forEach(card => {
-        card.style.backgroundImage = 'url("back.jpg")'
-    });
-}
 
-function anotherRound() {
+function turn() {
+    // if wait set, do nothing
     if (wait) {
         return
     }
+    // first card flip
     if (!flipped) {
         flipped = true
         firstCard = this
         flipImage(this)
+        // second card flip
     } else {
-        console.log(this.id, firstCard.id)
+        // check if first card was clicked again
         if (this.id == firstCard.id) {
             return
         }
+        // set wait to true, to block user from input, as long as it checks for match
         wait = true
         flipped = false
         secondCard = this
@@ -51,18 +82,34 @@ function anotherRound() {
     }
 }
 
+
+function flipImage(card) {
+    var name = card.getAttribute("name")
+    var url = "assets/img/" + name;
+    card.style.backgroundImage = 'url(' + url + ')'
+}
+
+
+function unflipAllImages() {
+    cards.forEach(card => {
+        card.style.backgroundImage = 'url("assets/img/back.jpg")'
+    });
+}
+
+
 function checkForMatch() {
     if (firstCard.getAttribute("name") == secondCard.getAttribute("name")) {
         firstCard.style.visibility = 'hidden'
-        firstCard.removeEventListener("click", anotherRound)
+        firstCard.removeEventListener("click", turn)
         secondCard.style.visibility = 'hidden'
-        secondCard.removeEventListener("click", anotherRound)
+        secondCard.removeEventListener("click", turn)
         checkForEnd()
     } else {
         unflipAllImages()
     }
     wait = false
 }
+
 
 function checkForEnd() {
     let gameOver = true
@@ -73,14 +120,6 @@ function checkForEnd() {
     })
     if (gameOver) {
         console.log("Winner! Game Over!")
-        alert("Du hast gewonnen :)")
+        $('#successModal').modal();
     }
 }
-
-function flipImage(card) {
-    var name = card.getAttribute("name")
-    var url = name + ".jpg"
-    card.style.backgroundImage = 'url(' + url + ')'
-}
-
-cards.forEach(card => card.addEventListener("click", anotherRound))
